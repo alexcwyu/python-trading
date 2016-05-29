a3e2c27ae16c8222d0b1df1dcf4016b08199bb21
alexcwyu <alexcwyu@gmail.com>
2016-05-29 21:57:50 +0800

    fix ATS runner

--- algotrader/samples/live_ats.py
+++ algotrader/samples/live_ats.py
@@ -3,33 +3,12 @@ Created on 4/16/16
 @author = 'jason'
 '''
 
-import time
-from algotrader.chart.plotter import StrategyPlotter
-from algotrader.provider.broker.simulator import Simulator
-from algotrader.provider.feed.csv import PandasCSVDataFeed
-from algotrader.strategy.down_2pct_strategy import Down2PctStrategy
+from algotrader.provider.broker.ib.ib_broker import IBBroker
 from algotrader.strategy.sma_strategy import SMAStrategy
+from algotrader.strategy.strategy import LiveTradingConfig
 from algotrader.trading.instrument_data import inst_data_mgr
 from algotrader.trading.order_mgr import order_mgr
 from algotrader.trading.portfolio import Portfolio
-from algotrader.utils import clock
-from algotrader.provider.broker.ib.ib_broker import IbGateway
-from algotrader.utils.betterthread import Thread as bThread
-from algotrader.utils.clock import default_clock
-from algotrader.utils import logger
-
-class StoppableSingleStrategyATS(bThread):
-    def run(self):
-        inst_data_mgr.start()
-        order_mgr.start()
-        self.ibgateway = IbGateway()
-        self.ibgateway.start()
-        logger.debug("[%s] Error: %s" % (self.__class__.__name__, "ibgateway started!"))
-        self.ibgateway.req_mktdata()
-
-    def shutdown(self):
-        del self.ibgateway
-
 
 
 class ATSRunner(object):
@@ -37,44 +16,31 @@ class ATSRunner(object):
         self.__stg = stg
 
     def start(self):
+        # clock.default_clock = clock.realtime_clock
         inst_data_mgr.start()
         order_mgr.start()
-        self.ibgateway = IbGateway()
-        self.ibgateway.start()
-        logger.debug("[%s] Error: %s" % (self.__class__.__name__, "ibgateway started!"))
-        self.ibgateway.req_mktdata()
+
+        self.__stg.start()
 
 
-# def main():
-    #feed = PandasCSVDataFeed(names=['spy'])
-    # portfolio = Portfolio(cash=100000)
-    #strategy = SMAStrategy("sma", Simulator.ID, feed, portfolio, instrument='spy', qty=1000)
-    #strategy = Down2PctStrategy("down2%", Simulator.ID, feed, portfolio, instrument='spy', qty=1000)
+def main():
+    portfolio = Portfolio(cash=100000)
+    broker = IBBroker()
 
-    # runner = SingleStrategyATSRunner(None)
-    # runner.start()
+    config = LiveTradingConfig(broker_id=IBBroker.ID,
+                               feed_id=IBBroker.ID,
+                               data_type=Bar,
+                               bar_type=BarType.Time,
+                               bar_size=BarSize.M1)
 
-    # try:
-    #     while True:
-    #         time.sleep(100000000000)
-    # except KeyboardInterrupt as e:
-    #     print "Keyboard Interupt recieved!"
+    strategy = SMAStrategy("sma", portfolio, instrument='spy', qty=1000, trading_config=config)
+    # strategy = Down2PctStrategy("down2%", portfolio, instrument='spy', qty=1000, trading_config=config)
+
+    runner = ATSRunner(strategy)
+    runner.start()
+
+    # TODO wait until stop
 
 
 if __name__ == "__main__":
-    # TODO: Call IB to get the account value
-    portfolio = Portfolio(cash=100000)
-    #t = StoppableSingleStrategyATS()
-    # t.start()
-
-
-    runner = ATSRunner(None)
-    runner.start()
-
-
-    print("Press <Ctrl-C> to stop thread")
-    while True:
-        try:
-            time.sleep(1000000)
-        except KeyboardInterrupt:
-            exit("Keyboard interrupt - waiting for thread to finish...")
+    main()

ccc80a39a6e80a791d0b760ecfc990bd3376155e
Jason Chan <cykjason1618@gmail.com>
2016-08-03 13:09:01 +0800

    add pair trading 	modified:   algotrader/provider/feed/pandas_memory.py 	new file:   algotrader/strategy/pair_trading.py 	new file:   poc/backtest_pairtrading.py

--- algotrader/provider/feed/pandas_memory.py
+++ algotrader/provider/feed/pandas_memory.py
@@ -28,11 +28,16 @@ class PandasMemoryDataFeed(Feed):
         self.dict_of_df = dict_of_df
         self.__ref_data_mgr = ref_data_mgr if ref_data_mgr else inmemory_ref_data_mgr
         self.__data_event_bus = data_event_bus if data_event_bus else EventBus.data_subject
+        self.__sub_keys = []
 
         feed_mgr.register(self)
 
     def start(self):
-        pass
+        self.__load_data(self.__sub_keys)
+        for index, row in self.df.iterrows():
+            ## TODO support bar filtering // from date, to date
+            bar = self.process_row(index, row)
+            self.__data_event_bus.on_next(bar)
 
     def stop(self):
         pass
@@ -41,10 +46,13 @@ class PandasMemoryDataFeed(Feed):
         return PandasMemoryDataFeed.ID
 
     def subscribe_all_mktdata(self, sub_keys):
-        self.__load_data([sub_keys])
+        if isinstance(sub_keys, list):
+            self.__sub_keys = self.__sub_keys + sub_keys
+        else:
+            self.__sub_keys.append(sub_keys)
 
     def subscribe_mktdata(self, sub_key):
-        self.__load_data([sub_key])
+        self.__sub_keys.append(sub_key)
 
     def process_row(self, index, row):
         inst = self.__ref_data_mgr.get_inst(symbol=row['Symbol'])
@@ -76,10 +84,10 @@ class PandasMemoryDataFeed(Feed):
 
         self.df = pd.concat(self.dfs).sort_index(0, ascending=True)
 
-        for index, row in self.df.iterrows():
-            ## TODO support bar filtering // from date, to date
-            bar = self.process_row(index, row)
-            self.__data_event_bus.on_next(bar)
+        # for index, row in self.df.iterrows():
+            # TODO support bar filtering // from date, to date
+            # bar = self.process_row(index, row)
+            # self.__data_event_bus.on_next(bar)
 
     def unsubscribe_mktdata(self, sub_key):
         pass
@@ -125,7 +133,7 @@ if __name__ == "__main__":
     #     df_dict = pickle.load(ifile)
 
     start_date = date(2000, 1, 1)
-    num_days = 1000
+    num_days = 500
     from datetime import timedelta
     import numpy as np
     import math
@@ -134,28 +142,49 @@ if __name__ == "__main__":
     x0 = 100
     dt = 1./252
 
-    dW = np.random.normal(0,math.sqrt(dt), num_days)
+    from algotrader.models.sde_sim import euler
+    drift0 = lambda x, t : 0.01*x
+    diffusion0 = lambda x, t: 0.05*x
+    drift1 = lambda x, t : -0.01*x
+    diffusion1 = lambda x, t: 0.05*x
+    drift2 = lambda x, t : -0.03*x
+    diffusion2 = lambda x, t: 0.08*x
 
-    asset = []
-    asset.append(x0)
-    for i in xrange(1, num_days):
-        xprev = asset[-1]
-        x = xprev + xprev*0.02*dt + sigma*xprev*dW[i]
-        asset.append(x)
+    simpath0 = euler(drift0, diffusion0, 1.0, 2.0, num_days, 1)
+    simpath1 = euler(drift1, diffusion1, 10.0, 2.0, num_days, 1)
+    simpath2 = euler(drift2, diffusion2, 100.0, 2.0, num_days, 1)
 
-    df = pd.DataFrame({"dates" : dates,
-                       "Open" : asset,
-                       "High" : asset,
-                       "Low" : asset,
-                       "Close" : asset,
-                       "Volume" : 10000*np.ones(num_days)})
+    df0 = pd.DataFrame({"dates": dates,
+                       "Open": simpath0[0,:],
+                       "High": simpath0[0,:],
+                       "Low": simpath0[0,:],
+                       "Close": simpath0[0,:],
+                       "Volume": 10000*np.ones(num_days)})
 
-    df = df.set_index(keys="dates")
+    df0 = df0.set_index(keys="dates")
 
-    df_dict = { 'SPY' : df,
-                'VXX' : df,
-                'XLV' : df,
-                'XIV' : df}
+    df1 = pd.DataFrame({"dates": dates,
+                        "Open": simpath1[0,:],
+                        "High": simpath1[0,:],
+                        "Low": simpath1[0,:],
+                        "Close": simpath1[0,:],
+                        "Volume": 10000*np.ones(num_days)})
+
+    df1 = df1.set_index(keys="dates")
+
+    df2 = pd.DataFrame({"dates": dates,
+                        "Open": simpath2[0,:],
+                        "High": simpath2[0,:],
+                        "Low": simpath2[0,:],
+                        "Close": simpath2[0,:],
+                        "Volume": 10000*np.ones(num_days)})
+
+    df2 = df2.set_index(keys="dates")
+
+
+    df_dict = {'SPY': df0,
+               'VXX': df1,
+               'XLV': df2}
 
     feed = PandasMemoryDataFeed(df_dict, ref_data_mgr=mgr)
 
@@ -164,10 +193,18 @@ if __name__ == "__main__":
     # sub_key = HistDataSubscriptionKey(inst_id=3, provider_id=PandasMemoryDataFeed.ID, data_type=Bar, bar_size=BarSize.D1,
     #                                   from_date=datetime(2010, 1, 1), to_date=today)
 
-    sub_key = HistDataSubscriptionKey(inst_id=3, provider_id=PandasMemoryDataFeed.ID, data_type=Bar, bar_size=BarSize.D1,
+    sub_key0 = HistDataSubscriptionKey(inst_id=0, provider_id=PandasMemoryDataFeed.ID, data_type=Bar, bar_size=BarSize.D1,
                                       from_date=dates[0], to_date=dates[-1])
+
+    sub_key1 = HistDataSubscriptionKey(inst_id=1, provider_id=PandasMemoryDataFeed.ID, data_type=Bar, bar_size=BarSize.D1,
+                                      from_date=dates[0], to_date=dates[-1])
+
+    sub_key2 = HistDataSubscriptionKey(inst_id=2, provider_id=PandasMemoryDataFeed.ID, data_type=Bar, bar_size=BarSize.D1,
+                                      from_date=dates[0], to_date=dates[-1])
+    feed.subscribe_all_mktdata([sub_key0, sub_key1, sub_key2])
 
     logger.setLevel(logging.DEBUG)
     eventLogger = EventLogger()
 
-    feed.subscribe_mktdata(sub_key)
+    feed.start()
+
---
+++ poc/backtest_pairtrading.py
@@ -0,0 +1,131 @@
+
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+from algotrader.chart.plotter import StrategyPlotter
+from algotrader.event.market_data import Bar, BarSize, BarType
+from algotrader.provider.feed.csv_feed import CSVDataFeed
+from algotrader.provider.feed.pandas_memory import PandasMemoryDataFeed
+from algotrader.provider.broker.sim.simulator import Simulator
+from algotrader.strategy.pair_trading import PairTradingWithOUSpread
+from algotrader.strategy.strategy import BacktestingConfig
+from algotrader.trading.instrument_data import inst_data_mgr
+from algotrader.trading.mock_ref_data import MockRefDataManager, build_inst_dataframe_from_list
+from algotrader.trading.order_mgr import order_mgr
+from algotrader.trading.portfolio import Portfolio
+from algotrader.utils import clock
+import pandas as pd
+import numpy as np
+import math
+from algotrader.models.sde_sim import euler
+
+
+class BacktestRunner(object):
+    def __init__(self, stg):
+        self.__stg = stg
+
+    def start(self):
+        clock.default_clock = clock.simluation_clock
+        clock.simluation_clock.start()
+        inst_data_mgr.start()
+        order_mgr.start()
+
+        self.__stg.start()
+
+
+def main():
+    symbols = ['XXX', 'YYY']
+
+    inst_df = build_inst_dataframe_from_list(symbols)
+    ccy_df = pd.DataFrame({ "ccy_id": ["USD", "HKD" ],
+                            "name": ["US Dollar", "HK Dollar"] })
+
+    exchange_df = pd.DataFrame({"exch_id": ["NYSE"],
+                                "name": ["New York Stock Exchange"]})
+
+    mgr = MockRefDataManager(inst_df=inst_df, ccy_df=ccy_df, exch_df=exchange_df)
+    portfolio = Portfolio(cash=100000)
+
+    start_date = datetime(2000, 1, 1)
+    num_days = 300
+
+    dates = [start_date + timedelta(days=i) for i in range(num_days)]
+
+    drift = lambda x, t: 0.02*x
+    diffusion = lambda x, t: 0.3*x
+
+    ou_k = 2.0
+    ou_theta = 0.25
+    ou_eta = 0.08
+
+    ou_drift = lambda x, t: ou_k*(ou_theta-x)
+    ou_diffusion =  lambda x, t: ou_eta*x
+
+    sim_asset_paths = euler(drift, diffusion, 100.0, 1.0, num_days, 10)
+    sim_spread_paths = euler(ou_drift, ou_diffusion, 0.1, 1.0, num_days, 10)
+    asset_x = sim_asset_paths[1,:]
+    spread = sim_spread_paths[1,:]
+    asset_y = np.exp(np.log(asset_x) + spread)
+
+    asset_x_df = pd.DataFrame({"dates": dates,
+                       "Open": asset_x,
+                       "High": asset_x,
+                       "Low": asset_x,
+                       "Close": asset_x,
+                       "Volume": 10000*np.ones(num_days)})
+
+    asset_y_df = pd.DataFrame({"dates": dates,
+                               "Open": asset_y,
+                               "High": asset_y,
+                               "Low": asset_y,
+                               "Close": asset_y,
+                               "Volume": 10000*np.ones(num_days)})
+
+    asset_x_df = asset_x_df.set_index(keys="dates")
+    asset_y_df = asset_y_df.set_index(keys="dates")
+
+    dict_df = {'XXX': asset_x_df,
+                'YYY': asset_y_df}
+
+    feed = PandasMemoryDataFeed(dict_df, ref_data_mgr=mgr)
+    broker = Simulator()
+    config = BacktestingConfig(broker_id=Simulator.ID,
+                               feed_id=PandasMemoryDataFeed.ID,
+                               data_type=Bar,
+                               bar_type=BarType.Time,
+                               bar_size=BarSize.D1,
+                               from_date=dates[0], to_date=dates[-1])
+
+    ou_params = {"k": ou_k,
+                 "theta": ou_theta,
+                 "eta": ou_eta}
+
+
+    strategy = PairTradingWithOUSpread("pairou", portfolio=portfolio,
+                                       instruments=[0, 1],
+                                       ou_params=ou_params,
+                                       gamma=1.0,
+                                       trading_config=config,
+                                       ref_data_mgr=mgr)
+
+    runner = BacktestRunner(strategy)
+    runner.start()
+    print portfolio.get_result()
+
+    # pyfolio
+    rets = strategy.get_portfolio().get_return()
+    # import pyfolio as pf
+    # pf.create_returns_tear_sheet(rets)
+    # pf.create_full_tear_sheet(rets)
+
+    # build in plot
+    plotter = StrategyPlotter(strategy)
+    plotter.plot(instrument=0)
+
+    #import matplotlib.pyplot as plt
+    #plt.show()
+
+
+if __name__ == "__main__":
+    main()

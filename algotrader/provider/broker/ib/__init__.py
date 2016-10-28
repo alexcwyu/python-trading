7689713e51f2c52a0a6759f3f990c3c155250ca5
alexcwyu <alexcwyu@gmail.com>
2016-10-29 00:09:12 +0800

    clean up

--- algotrader/provider/broker/ib/__init__.py
+++ algotrader/provider/broker/ib/__init__.py
@@ -1,14 +1,6 @@
-
-
 from collections import namedtuple
-
-#OrderAttribute
-
-
 
 OrderAttribute = namedtuple('TimeInForce',
                             ['DAY', 'GTC', 'OPG', 'IOC', 'GTD', 'FOK', 'DTC'])
 
 time_in_force = OrderAttribute('DAY', 'GTC', 'OPG', 'IOC', 'GTD', 'FOK', 'DTC')
-
-

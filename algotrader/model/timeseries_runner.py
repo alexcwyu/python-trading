06f18d4afda02277a7253b4c841404b34a07501a
alexcwyu <alexcwyu@gmail.com>
2017-08-03 23:05:25 +0800

    data frame

--- algotrader/model/timeseries_runner.py
+++ algotrader/model/timeseries_runner.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import raccoon as rc
 from rx.subjects import Subject
+from algotrader import Startable, Context
 
 import algotrader.model.time_series2_pb2 as proto
 from algotrader.model.model_factory import ModelFactory
@@ -81,7 +82,7 @@ def from_np_type(dtype: np.dtype) -> int:
 
 class Series(rc.Series):
     def __init__(self, proto_series: proto.Series = None,
-                 series_id: str = None, parent_id: str = None,
+                 series_id: str = None, df_id: str = None,
                  col_id: str = None, inst_id: str = None, dtype: str = None):
         if not proto_series:
             # TODO try get the protoseries by series_id from DB
@@ -93,7 +94,7 @@ class Series(rc.Series):
                                          data_name=proto_series.col_id,
                                          index_name="timestamp", use_blist=True)
             self.series_id = proto_series.series_id
-            self.parent_id = proto_series.parent_id
+            self.df_id = proto_series.df_id
             self.col_id = proto_series.col_id
             self.inst_id = proto_series.inst_id
             self.dtype = to_np_type(proto_series.dtype)
@@ -101,7 +102,7 @@ class Series(rc.Series):
             super(Series, self).__init__(data_name=col_id, index_name="timestamp", use_blist=True)
 
             self.series_id = series_id
-            self.parent_id = parent_id
+            self.df_id = df_id
             self.col_id = col_id
             self.inst_id = inst_id
             self.dtype = dtype
@@ -121,8 +122,8 @@ class Series(rc.Series):
                                                         data={self.col_id: value}))
 
 
-class DataFrame:
-    def __init__(self, parent_id: str, inst_id: str, series_list=None, series_ids=None, col_ids=None, dtypes=None):
+class DataFrame(Startable):
+    def __init__(self, df_id: str, inst_id: str, series_list=None, series_ids=None, col_ids=None, dtypes=None):
 
         if not series_list:
             series_list = []
@@ -130,7 +131,7 @@ class DataFrame:
             series_list = [series_list]
 
         if not series_ids:
-            series_ids = []
+            series_ids = [series.series_id for series in series_list]
         elif not isinstance(series_ids, (set, tuple, list)):
             series_ids = [series_ids]
 
@@ -155,16 +156,19 @@ class DataFrame:
                     else:
                         dtype = dtypes[idx]
 
-                series = Series(series_id=series_id, parent_id=parent_id, col_id=col_id, inst_id=inst_id, dtype=dtype)
+                series = Series(series_id=series_id, df_id=df_id, col_id=col_id, inst_id=inst_id, dtype=dtype)
 
             if isinstance(series, proto.Series):
+                series = Series(proto_series=series)
+            elif isinstance(series, pd.Series):
                 series = Series(proto_series=series)
 
             self.series_dict[series.series_id] = series
             self.col_dict[series.col_id] = series
 
-        self.parent_id = parent_id
+        self.df_id = df_id
         self.inst_id = inst_id
+        self.time_list = []
         self.subject = Subject()
 
     def on_update(self, event: TimeSeriesUpdateEvent):
@@ -173,11 +177,35 @@ class DataFrame:
     def _process_update(self, source, timestamp, data: Dict):
         pass
 
+
+    def _start(self, app_context: Context) -> None:
+        pass
+
+    def _stop(self):
+        pass
+
+    def id(self):
+        self.df_id
+
     def add(self, timestamp, data: Dict):
+        self.time_list.append(timestamp)
         for col_id, value in data.items():
             self.col_dict[col_id].add(timestamp=timestamp, value=value)
         self.subject.on_next(
-            ModelFactory.build_time_series_update_event(source=self.parent_id, timestamp=timestamp, data=data))
+            ModelFactory.build_time_series_update_event(source=self.df_id, timestamp=timestamp, data=data))
+
+
+    def get_last_update_time(self):
+        return self.time_list[-1]
+
+
+    def has_update(self, timestamp):
+        # TODO check update time
+        return self.time_list and self.time_list[-1] > timestamp
+
+    def get_data_since(self, timestamp):
+        # TODO
+        pass
 
 
 # class DataFrame(rc.DataFrame):
@@ -207,7 +235,7 @@ class DataFrame:
 def series_to_proto_series(series: Series) -> proto.Series:
     proto_series = proto.Series()
     proto_series.id = series.id
-    proto_series.parent_id = series.parent_id
+    proto_series.df_id = series.df_id
     proto_series.col_id = series.col_id
     proto_series.inst_id = series.inst_id
     proto_series.dtype = from_np_type(series.dtype)
@@ -220,8 +248,8 @@ def series_to_pd_series(series: Series) -> pd.Series:
     data = series._data
     index = series._index
     pd_series = pd.Series(data=data, index=index, name=series.data_name, dtype=series.dtype)
-    pd_series.id = series.id
-    pd_series.parent_id = series.parent_id
+    pd_series.series_id = series.series_id
+    pd_series.df_id = series.df_id
     pd_series.col_id = series.col_id
     pd_series.inst_id = series.inst_id
     return pd_series
@@ -233,10 +261,10 @@ def pd_series_to_series(pd_series: pd.Series) -> Series:
     series.data_name = pd_series.name
     series.dtype = pd_series.dtype
 
-    if hasattr(pd_series, 'id'):
-        series.id = pd_series.id
-    if hasattr(pd_series, 'parent_id'):
-        series.parent_id = pd_series.parent_id
+    if hasattr(pd_series, 'series_id'):
+        series.series_id = pd_series.series_id
+    if hasattr(pd_series, 'df_id'):
+        series.df_id = pd_series.df_id
     if hasattr(pd_series, 'col_id'):
         series.col_id = pd_series.col_id
     if hasattr(pd_series, 'inst_id'):
@@ -251,16 +279,16 @@ def proto_series_to_series(proto_series: proto.Series) -> Series:
     series.data_name = proto_series.col_id
     series.dtype = to_np_type(proto_series.dtype)
 
-    series.id = proto_series.id
-    series.parent_id = proto_series.parent_id
+    series.series_id = proto_series.series_id
+    series.df_id = proto_series.df_id
     series.col_id = proto_series.col_id
     series.inst_id = proto_series.inst_id
     return series
 
 
 def df_to_proto_series(dataframe: DataFrame) -> Dict[str, proto.Series]:
-    data_dict = dataframe.to_dict(index=False)
-    index = dataframe.index
+    data_dict = dataframe.series_dict
+    #index = dataframe.index
     result = {}
     for k, v in data_dict.items():
         series = series_to_proto_series(v)
@@ -269,36 +297,50 @@ def df_to_proto_series(dataframe: DataFrame) -> Dict[str, proto.Series]:
 
 
 def df_to_pd_df(dataframe: DataFrame) -> pd.DataFrame:
-    data_dict = dataframe.to_dict(index=False)
-    return pd.DataFrame(data_dict, columns=dataframe.columns, index=dataframe.index)
+    data_dict = { k : series_to_pd_series(v) for k , v in dataframe.series_dict.items()}
+    df = pd.DataFrame(data_dict)
+    df.df_id = dataframe.df_id
+    df.inst_id = dataframe.inst_id
+    return df
 
 
 def pd_df_to_df(pd_dataframe: pd.DataFrame) -> DataFrame:
     columns = pd_dataframe.columns.tolist()
-    data = dict()
+    #data = dict()
     pandas_data = pd_dataframe.values.T.tolist()
-    for i in range(len(columns)):
-        data[columns[i]] = pandas_data[i]
-    index = pd_dataframe.index.tolist()
-    index_name = pd_dataframe.index.name
-    index_name = 'index' if not index_name else index_name
-    return rc.DataFrame(data=data, columns=columns, index=index, index_name=index_name)
+    series_list = [pd_series_to_series(data) for data in pandas_data]
 
+    #for i in range(len(columns)):
+    #    data[columns[i]] = pandas_data[i]
+    #index = pd_dataframe.index.tolist()
+    #index_name = pd_dataframe.index.name
+    #index_name = 'index' if not index_name else index_name
+
+    if hasattr(pd_dataframe, 'df_id'):
+        df_id = pd_dataframe.df_id
+
+    if hasattr(pd_dataframe, 'inst_id'):
+        inst_id = pd_dataframe.inst_id
+
+    return DataFrame(df_id=df_id, inst_id=inst_id, series_list=series_list)
+
+df_id = "Bar.Daily"
+inst_id ="HSI@SEHK"
 
 proto_series1 = proto.Series()
-proto_series1.id = "Bar.Daily.close-HSI@SEHK"
-proto_series1.parent_id = "Bar.Daily"
+proto_series1.series_id = "Bar.Daily.close-HSI@SEHK"
+proto_series1.df_id = df_id
 proto_series1.col_id = "close"
-proto_series1.inst_id = "HSI@SEHK"
+proto_series1.inst_id = inst_id
 proto_series1.dtype = proto.DTDouble
 proto_series1.index.extend(list(range(1499787464853, 1499887464853, 20000000)))
 proto_series1.double_data.extend(np.random.rand(5))
 
 proto_series2 = proto.Series()
-proto_series2.id = "Bar.Daily.open-HSI@SEHK"
-proto_series2.parent_id = "Bar.Daily"
+proto_series2.series_id = "Bar.Daily.open-HSI@SEHK"
+proto_series2.df_id = df_id
 proto_series2.col_id = "open"
-proto_series2.inst_id = "HSI@SEHK"
+proto_series2.inst_id = inst_id
 proto_series2.dtype = proto.DTDouble
 proto_series2.index.extend(list(range(1499787464853, 1499887464853, 10000000)))
 proto_series2.double_data.extend(np.random.rand(10))
@@ -312,19 +354,19 @@ pd_series_2 = series_to_pd_series(series2)
 series1_1 = pd_series_to_series(pd_series_1)
 series2_1 = pd_series_to_series(pd_series_2)
 
-df = DataFrame([series1_1, series2_1])
+df = DataFrame(df_id=df_id, inst_id = inst_id, series_list=[series1_1, series2_1])
 
 pd_df = df_to_pd_df(df)
 
 df2 = pd_df_to_df(pd_df)
 
-print(df)
+#print(df)
 print(pd_df)
-print(df2)
+#print(df2)
 
 result = df_to_proto_series(df2)
 
-print(result)
+#print(result)
 
 #
 #
